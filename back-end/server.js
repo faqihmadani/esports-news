9e2f0b31127dfcd053b917a4b4d1c52b3057fbb1
Faqih Madani <faqihfaqih22@gmail.com>
2023-12-27 23:50:36 +0700

    membuat kerangka backend

--- back-end/server.js
+++ back-end/server.js
@@ -1,1 +1,17 @@
-console.log("Server");
+import express from "express";
+
+import userRoutes from "./routes/UserRoute.js"
+import postRoutes from "./routes/PostRoute.js"
+import authRoutes from "./routes/AuthRoute.js"
+
+const app = express();
+
+app.use("/api/users", userRoutes)
+app.use(postRoutes)
+app.use(authRoutes)
+
+app.use(express.json())
+app.listen(8800, () => {
+    console.log("Connected in port 8800");
+})
+
